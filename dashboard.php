64b4c578a23d5829c59f916d361e1c18493a1ed8
Dinesh Kumar Choudhary <mapmydestination@gmail.com>
2021-01-31 21:26:51 +0530

    update

--- dashboard.php
+++ dashboard.php
@@ -25,12 +25,12 @@ else{
         <!-- Styles -->
         <link type="text/css" rel="stylesheet" href="../assets/plugins/materialize/css/materialize.min.css"/>
         <link href="http://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">    
-        <link href="../assets/plugins/metrojs/MetroJs.min.css" rel="stylesheet">
-        <link href="../assets/plugins/weather-icons-master/css/weather-icons.min.css" rel="stylesheet">
+        <link href="assets/plugins/metrojs/MetroJs.min.css" rel="stylesheet">
+        <link href="assets/plugins/weather-icons-master/css/weather-icons.min.css" rel="stylesheet">
         	
         <!-- Theme Styles -->
-        <link href="../assets/css/alpha.min.css" rel="stylesheet" type="text/css"/>
-        <link href="../assets/css/custom.css" rel="stylesheet" type="text/css"/>
+        <link href="assets/css/alpha.min.css" rel="stylesheet" type="text/css"/>
+        <link href="assets/css/custom.css" rel="stylesheet" type="text/css"/>
         
     </head>
     <body>
@@ -163,23 +163,23 @@ if($stats==1){
         </div>       
         
         <!-- Javascripts -->
-        <script src="../assets/plugins/jquery/jquery-2.2.0.min.js"></script>
-        <script src="../assets/plugins/materialize/js/materialize.min.js"></script>
-        <script src="../assets/plugins/material-preloader/js/materialPreloader.min.js"></script>
-        <script src="../assets/plugins/jquery-blockui/jquery.blockui.js"></script>
-        <script src="../assets/plugins/waypoints/jquery.waypoints.min.js"></script>
-        <script src="../assets/plugins/counter-up-master/jquery.counterup.min.js"></script>
-        <script src="../assets/plugins/jquery-sparkline/jquery.sparkline.min.js"></script>
-        <script src="../assets/plugins/chart.js/chart.min.js"></script>
-        <script src="../assets/plugins/flot/jquery.flot.min.js"></script>
-        <script src="../assets/plugins/flot/jquery.flot.time.min.js"></script>
-        <script src="../assets/plugins/flot/jquery.flot.symbol.min.js"></script>
-        <script src="../assets/plugins/flot/jquery.flot.resize.min.js"></script>
-        <script src="../assets/plugins/flot/jquery.flot.tooltip.min.js"></script>
-        <script src="../assets/plugins/curvedlines/curvedLines.js"></script>
-        <script src="../assets/plugins/peity/jquery.peity.min.js"></script>
-        <script src="../assets/js/alpha.min.js"></script>
-        <script src="../assets/js/pages/dashboard.js"></script>
+        <script src="assets/plugins/jquery/jquery-2.2.0.min.js"></script>
+        <script src="assets/plugins/materialize/js/materialize.min.js"></script>
+        <script src="assets/plugins/material-preloader/js/materialPreloader.min.js"></script>
+        <script src="assets/plugins/jquery-blockui/jquery.blockui.js"></script>
+        <script src="assets/plugins/waypoints/jquery.waypoints.min.js"></script>
+        <script src="assets/plugins/counter-up-master/jquery.counterup.min.js"></script>
+        <script src="assets/plugins/jquery-sparkline/jquery.sparkline.min.js"></script>
+        <script src="assets/plugins/chart.js/chart.min.js"></script>
+        <script src="assets/plugins/flot/jquery.flot.min.js"></script>
+        <script src="assets/plugins/flot/jquery.flot.time.min.js"></script>
+        <script src="assets/plugins/flot/jquery.flot.symbol.min.js"></script>
+        <script src="assets/plugins/flot/jquery.flot.resize.min.js"></script>
+        <script src="assets/plugins/flot/jquery.flot.tooltip.min.js"></script>
+        <script src="assets/plugins/curvedlines/curvedLines.js"></script>
+        <script src="assets/plugins/peity/jquery.peity.min.js"></script>
+        <script src="assets/js/alpha.min.js"></script>
+        <script src="assets/js/pages/dashboard.js"></script>
         
     </body>
 </html>
